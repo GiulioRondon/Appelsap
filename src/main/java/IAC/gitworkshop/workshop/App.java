e3e68a4c216f14bf7c285f40dfa45cbfbac84b50
Giulio <giulio.rondon@student.hu.nl>
2019-02-26 13:46:32 +0100

    YAAAAAAW

---
+++ src/main/java/IAC/gitworkshop/workshop/App.java
@@ -0,0 +1,13 @@
+package IAC.gitworkshop.workshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
